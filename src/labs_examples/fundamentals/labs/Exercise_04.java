39af0af503e9cc3a8b94a74094d46e5efa55efd0
evanrobert <evan.lindsay223@gmail.com>
2022-09-10 11:42:27 -0700

    Exercise_04.java

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,33 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        char l = 'a';
+        System.out.println("a");
+
+
+//
+        boolean d = true;
+        System.out.println(d);
+        //
+        int sum = 200000 +2;
+        System.out.println("the result is" +sum);
+        //
+        double b = 122.25;
+        double a =100.25;
+        System.out.println(a+b);
+
+//
+        float x = 12000000f;
+        System.out.println(x);
+//
+        long y = 9000000l;
+        System.out.println(y);
+        //
+        short q = 32000;
+        System.out.println(q);
+        //
+        byte z =126;
+        System.out.println(z);
 
     }
 
