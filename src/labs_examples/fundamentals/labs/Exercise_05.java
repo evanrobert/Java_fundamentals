6abb7d15813586ba4002e5fdf3bffa06c10e6903
evanrobert <evan.lindsay223@gmail.com>
2022-09-12 20:59:14 -0700

    Moved my second example.

--- src/labs_examples/fundamentals/labs/Exercise_05.java
+++ src/labs_examples/fundamentals/labs/Exercise_05.java
@@ -14,16 +14,23 @@ public class Exercise_05 {
 
         String str = "hello!";
         // please declare an int variable below, and set it to the value of the length of "str"
+        int length = "hello!".length();
 
         String str2 = "hello";
         // please initialize a boolean variable and test whether str is equal to str2
-
+        boolean equals = "hello".equals("hello");
         // please concatenate str & str2 and set the result to a new String variable below
-
+        int variable = 10;
+        String str = "hello" + "the value of variable is" + variable + "!";
+        system.out.println(str);
         // please demonstrate the use of any other method that is available to us in the String class
         // for example, replace(), substring(), contains(), indexOf() etc
+    }}
+    String subStr = str1.substring(4, 8);
+        System.out.println("The substring is: " + subStr)
 
-    }
 
 
-}
+
+
+
