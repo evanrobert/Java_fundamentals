80046442107c7f2f4a2380cac11d15c264f27952
evanrobert <evan.lindsay223@gmail.com>
2022-11-26 11:25:09 -0800

    completed lab 5 in exceptions

--- src/labs_examples/exception_handling/labs/Exercise_05.java
+++ src/labs_examples/exception_handling/labs/Exercise_05.java
@@ -1,7 +1,5 @@
 package labs_examples.exception_handling.labs;
 
-import java.util.Arrays;
-
 /**
  * Exception Handling Exercise 5:
  *
@@ -10,9 +8,28 @@ import java.util.Arrays;
  */
 
 class Example {
-
     public static void main(String[] args) {
+        try {
+            one();
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Exception was caught ");
 
+        }
     }
 
+    public static void one() throws ArrayIndexOutOfBoundsException {
+        two();
+    }
+
+
+    public static void two() throws ArrayIndexOutOfBoundsException {
+        int[] coolArray = {2, 5, 8, 10, 12 };
+        try {
+            int a = coolArray[10];
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Caught in two method ");
+            System.out.println("throwing back up to main method");
+            throw exc;
+        }
+    }
 }
